063f75c4f5bdcf6191e00b59b79bfdd97d511f12
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2024-11-30 17:14:13 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Contador from './assets/components/Contador'
+function App() {
+  return (
+    <>
+      <img src="https://www.rj.senac.br/wp-content/webp-express/webp-images/wp-content/themes/senac-def-2021/images/logo.png.webp" alt="Logotipo do Senac RJ" />
+      <h1>Contador com React</h1>
+      
+      <Contador/>
+    </>
+  )
+}
+export default App
